d13d15a546ed2f132e926916ccc18d0294dd68dc
onakamaruido-source <onaka.marui.do@gmail.com>
2026-04-20 18:13:37 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-
-menuBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("active");
-});
